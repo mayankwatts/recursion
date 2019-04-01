22353a00dbf666ef83eeaf22f647937fd5fef8e3
mayankwatts <47928731+mayankwatts@users.noreply.github.com>
2019-04-02 00:17:44 +0530

    Create Last Index of Number.cpp

---
+++ Last Index of Number.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int lastIndex(int input[], int size, int x) {
+  if(size==0)
+  {
+    return -1;
+  }
+  
+  int out=lastIndex(input+1,size-1,x);
+  
+  if(input[0]==x && out==-1)
+  {
+    return out+1;
+  }
+  
+  if(out==-1)
+  {
+    return out;
+  }
+  if(out!=-1)
+  {
+    return out+1;
+  }
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    cout << lastIndex(input, n, x) << endl;
+    
+    return 0;
+
+}
+
+
