007ca63995b0c92cd5aa3bf0f578090702cf58a0
mayankwatts <47928731+mayankwatts@users.noreply.github.com>
2019-04-02 00:19:27 +0530

    Create All Indices of Number.cpp

---
+++ All Indices of Number.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+
+int allIndexes(int input[], int size, int x, int output[]) {
+  
+  if(size==0)
+  {
+    return 0;
+  }
+
+  int out=allIndexes(input,size-1,x,output);
+  if(input[size-1]==x)
+  {
+    output[out]=size-1;
+    return out+1;
+  }
+  return out;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    int *output = new int[n];
+    
+    int size = allIndexes(input, n, x, output);
+    for(int i = 0; i < size; i++) {
+        cout << output[i] << " ";
+    }
+    
+    delete [] input;
+    
+    delete [] output;
+    
+    return 0; 
+
+}
+
+
